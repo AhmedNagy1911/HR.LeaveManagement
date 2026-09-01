672c62fc1f7e3b583406b50df3b031872d8fc632
Ahmed Nagy <zamalekahmed196@gmail.com>
2026-09-02 00:47:24 +0300

    Add Entity Framework and Database Context

--- HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
+++ HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using HR.LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace HR.LeaveManagement.Persistence;
@@ -8,6 +10,9 @@ public static class PersistenceServiceRegistration
     public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
         IConfiguration configuration)
     {
+        services.AddDbContext<HrDatabaseContext>(options => {
+            options.UseSqlServer(configuration.GetConnectionString("HrDatabaseConnectionString"));
+        });
 
         return services;
     }
