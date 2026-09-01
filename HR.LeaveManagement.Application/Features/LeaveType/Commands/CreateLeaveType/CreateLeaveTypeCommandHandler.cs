89e085aefb9687dac67ba3f62ac46978ff63344a
Ahmed Nagy <zamalekahmed196@gmail.com>
2026-09-01 20:18:07 +0300

    Creating CQRS Command Request and Handler

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
+
+public class CreateLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository, Mapper mapper)
+    : IRequestHandler<CreateLeaveTypeCommand, int>
+{
+    private readonly ILeaveTypeRepository _leavetyperepository = leaveTypeRepository;
+    private readonly Mapper _mapper = mapper;
+
+    public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
+    {
+
+        // convert to domain entity object
+        var leaveTypeToCreate = _mapper.Map<Domain.LeaveType>(request);
+
+        // add to database
+        await _leavetyperepository.CreateAsync(leaveTypeToCreate);
+
+        // retun record id
+        return leaveTypeToCreate.Id;
+    }
+}
