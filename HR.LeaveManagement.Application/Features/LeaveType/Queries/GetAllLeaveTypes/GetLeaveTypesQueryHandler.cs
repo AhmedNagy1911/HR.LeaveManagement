3a429a56801941b6e88c4c166591b3a76399b32e
Ahmed Nagy <zamalekahmed196@gmail.com>
2026-09-01 18:24:05 +0300

    Creating CQRS Query Request and Handler

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/GetLeaveTypesQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+
+public class GetLeaveTypesQueryHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+    : IRequestHandler<GetLeaveTypesQuery, List<LeaveTypeDto>>
+{
+    private readonly ILeaveTypeRepository _leaveTypeRepository = leaveTypeRepository;
+    private readonly IMapper _mapper = mapper;
+
+    public async Task<List<LeaveTypeDto>> Handle(GetLeaveTypesQuery request, CancellationToken cancellationToken)
+    {
+        // Query the database
+        var leaveTypes = await _leaveTypeRepository.GetAsync();
+
+        // convert data objects to DTO objects
+        var data = _mapper.Map<List<LeaveTypeDto>>(leaveTypes);
+
+        // return list of DTO object
+        return data;
+    }
+}
