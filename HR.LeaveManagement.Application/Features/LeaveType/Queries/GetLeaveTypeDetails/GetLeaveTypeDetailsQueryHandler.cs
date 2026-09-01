cf7ac2d59445d5253203eb1f9749a8212fad2841
Ahmed Nagy <zamalekahmed196@gmail.com>
2026-09-01 18:45:00 +0300

    Creating GetLeaveTypeDetails CQRS Query Request and Handler

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Queries/GetLeaveTypeDetails/GetLeaveTypeDetailsQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
+
+public class GetLeaveTypeDetailsQueryHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper) 
+    : IRequestHandler<GetLeaveTypeDetailsQuery, LeaveTypeDetailDto>
+{
+    private readonly ILeaveTypeRepository _leaveTypeRepository = leaveTypeRepository;
+    private readonly IMapper _mapper = mapper;
+
+    public async Task<LeaveTypeDetailDto> Handle(GetLeaveTypeDetailsQuery request, CancellationToken cancellationToken)
+    {
+        var leaveType = await _leaveTypeRepository.GetByIdAsync(request.Id);
+
+        return _mapper.Map<LeaveTypeDetailDto>(leaveType);
+    }
+}
