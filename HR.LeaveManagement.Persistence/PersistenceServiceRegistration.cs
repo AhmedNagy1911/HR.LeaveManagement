a0204d253c501d0ea506bd6c28639e825c333e00
Ahmed Nagy <zamalekahmed196@gmail.com>
2026-09-02 00:25:55 +0300

    Create the Persistence Project

---
+++ HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+
+        return services;
+    }
+}
