d0d436d2b80b1ac26c794a05262c33dd65fd9e9a
Ahmed Nagy <zamalekahmed196@gmail.com>
2026-09-02 00:04:19 +0300

    Adding FluentValidation

--- HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/CreateLeaveType/CreateLeaveTypeCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
 using MediatR;
 
 namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
@@ -13,7 +14,12 @@ public class CreateLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeReposit
     public async Task<int> Handle(CreateLeaveTypeCommand request, CancellationToken cancellationToken)
     {
         // Validate incoming data
+        // Validate incoming data
+        var validator = new CreateLeaveTypeCommandValidator(_leavetyperepository);
+        var validationResult = await validator.ValidateAsync(request);
 
+        if (validationResult.Errors.Any())
+            throw new BadRequestException("Invalid Leave type", validationResult);
 
 
         // convert to domain entity object
