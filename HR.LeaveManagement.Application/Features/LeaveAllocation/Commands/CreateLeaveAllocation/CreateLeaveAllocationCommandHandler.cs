4bcc043ccc34b6402e1cfcbea7b9a03e661922b0
Ahmed Nagy <zamalekahmed196@gmail.com>
2026-09-03 17:57:32 +0300

    feat: Complete Leave Allocation Feature

---
+++ HR.LeaveManagement.Application/Features/LeaveAllocation/Commands/CreateLeaveAllocation/CreateLeaveAllocationCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Exceptions;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.CreateLeaveAllocation
+{
+    public class CreateLeaveAllocationCommandHandler(IMapper mapper
+        ,ILeaveAllocationRepository leaveAllocationRepository
+        , ILeaveTypeRepository leaveTypeRepository)
+        : IRequestHandler<CreateLeaveAllocationCommand, Unit>
+    {
+        private readonly IMapper _mapper = mapper;
+        private readonly ILeaveAllocationRepository _leaveAllocationRepository = leaveAllocationRepository;
+        private readonly ILeaveTypeRepository _leaveTypeRepository = leaveTypeRepository;
+
+        public async Task<Unit> Handle(CreateLeaveAllocationCommand request, CancellationToken cancellationToken)
+        {
+            var validator = new CreateLeaveAllocationCommandValidator(_leaveTypeRepository);
+            var validationResult = await validator.ValidateAsync(request);
+
+            if (validationResult.Errors.Any())
+                throw new BadRequestException("Invalid Leave Allocation Request", validationResult);
+
+            // Get Leave type for allocations
+            var leaveType = await _leaveTypeRepository.GetByIdAsync(request.LeaveTypeId);
+            var leaveAllocation = _mapper.Map<Domain.LeaveAllocation>(request);
+            await _leaveAllocationRepository.CreateAsync(leaveAllocation);
+
+            return Unit.Value;
+        }
+    }
+}
--- HR.LeaveManagement.Application/MappingProfiles/LeaveAllocationProfile.cs
+++ HR.LeaveManagement.Application/MappingProfiles/LeaveAllocationProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.Commands.CreateLeaveAllocation;
 using HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocationDetails;
 using HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocations;
+using HR.LeaveManagement.Application.Features.LeaveAllocations.Requests.Commands;
 using HR.LeaveManagement.Domain;
 
 namespace HR.LeaveManagement.Application.MappingProfiles;
@@ -11,5 +13,7 @@ public class LeaveAllocationProfile : Profile
     {
         CreateMap<LeaveAllocation, LeaveAllocationDetailsDto>();
         CreateMap<LeaveAllocationDto, LeaveAllocation>().ReverseMap();
+        CreateMap<CreateLeaveAllocationCommand, LeaveAllocation>();
+        CreateMap<UpdateLeaveAllocationCommand, LeaveAllocation>();
     }
 }
