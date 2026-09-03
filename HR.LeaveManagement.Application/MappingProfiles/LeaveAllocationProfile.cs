307a8cb717ee876ddde8c615207a1108df8dcf2e
Ahmed Nagy <zamalekahmed196@gmail.com>
2026-09-03 17:08:45 +0300

    feat: add GetLeaveAllocationDetailRequestHandler

---
+++ HR.LeaveManagement.Application/MappingProfiles/LeaveAllocationProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.Queries.GetLeaveAllocationDetails;
+using HR.LeaveManagement.Domain;
+
+namespace HR.LeaveManagement.Application.MappingProfiles;
+
+public class LeaveAllocationProfile : Profile
+{
+    public LeaveAllocationProfile()
+    {
+        CreateMap<LeaveAllocation, LeaveAllocationDetailsDto>();
+    }
+}
