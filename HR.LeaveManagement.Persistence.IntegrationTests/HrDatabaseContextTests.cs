bc6ef0be96d52e49270f170366d111263ecd8169
Ahmed Nagy <zamalekahmed196@gmail.com>
2026-09-05 03:07:31 +0300

    feat: Integration Testing - Entity Framework

---
+++ HR.LeaveManagement.Persistence.IntegrationTests/HrDatabaseContextTests.cs
@@ -0,0 +1,57 @@
+﻿using HR.LeaveManagement.Domain;
+using HR.LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Shouldly;
+
+namespace HR.LeaveManagement.Persistence.IntegrationTests;
+
+public class HrDatabaseContextTests
+{
+    private HrDatabaseContext _hrDatabaseContext;
+
+    public HrDatabaseContextTests()
+    {
+        var dbOptions = new DbContextOptionsBuilder<HrDatabaseContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+        _hrDatabaseContext = new HrDatabaseContext(dbOptions);
+    }
+
+    [Fact]
+    public async Task Save_SetDateCreatedValue()
+    {
+        // Arrange
+        var leaveType = new LeaveType
+        {
+            Id = 1,
+            DefaultDays = 10,
+            Name = "Test Vacation"
+        };
+
+        // Act
+        await _hrDatabaseContext.LeaveTypes.AddAsync(leaveType, TestContext.Current.CancellationToken);
+        await _hrDatabaseContext.SaveChangesAsync(TestContext.Current.CancellationToken);
+
+        // Assert
+        leaveType.DateCreated.ShouldNotBeNull();
+    }
+
+    [Fact]
+    public async Task Save_SetDateModifiedValue()
+    {
+        // Arrange
+        var leaveType = new LeaveType
+        {
+            Id = 1,
+            DefaultDays = 10,
+            Name = "Test Vacation"
+        };
+
+        // Act
+        await _hrDatabaseContext.LeaveTypes.AddAsync(leaveType, TestContext.Current.CancellationToken);
+        await _hrDatabaseContext.SaveChangesAsync(TestContext.Current.CancellationToken);
+
+        // Assert
+        leaveType.DateModified.ShouldNotBeNull();
+    }
+}
